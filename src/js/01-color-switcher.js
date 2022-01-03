f1a217bf13da10f0a202e079e4bb1746638b89a4
Iryna Hudymenko <iryn4ik09@gmail.com>
2022-01-03 23:45:41 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let onClick = null;
+let onDisabled = null;
+
+buttonStart.addEventListener('click', () => {
+  onClick = setInterval(() => {
+    onChangeColorBody()
+  }, 1000);
+});
+function onChangeColorBody() {
+    let colorFunction = getRandomHexColor();
+     document.body.style.backgroundColor = colorFunction;
+}
+buttonStart.addEventListener('click', () => {
+  onDisabled = setTimeout(() => {
+    buttonStart.setAttribute("disabled", "disabled")
+  }, 1000);
+});
+buttonStop.addEventListener('click', () => {
+    clearInterval(onClick);
+     buttonStart.removeAttribute("disabled", "disabled")
+  console.log("Цвет не меняется и кнопка снова включается");  
+});
 
