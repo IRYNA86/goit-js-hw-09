bcab2c7f2ac235d92d3b5555b3cfbd58b41bb21a
Iryna Hudymenko <iryn4ik09@gmail.com>
2022-01-09 01:10:20 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+const formLabel = document.querySelector('.form');
+formLabel.addEventListener('submit', onButtonSubmit);
+let position = 0;
+function onButtonSubmit(event) {
+  event.preventDefault();
+  let delay = Number(event.currentTarget.delay.value);
+  let step = Number(event.currentTarget.step.value);
+  let amount = Number(event.currentTarget.amount.value);
+  setInterval(() => {
+    if (position >= amount) {
+      return;
+    }
+    position += 1;
+    setTimeout(() => {
+      delay += step;
+    });
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }, step);
 }
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    setInterval(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      reject({ position, delay });
+    }, delay);
+  });
+  return promise;
+  }
